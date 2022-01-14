464f3cc3c5ef47bc842ce853736d72fac46949f0
이지호 <42643691+jiho9702@users.noreply.github.com>
2022-01-14 17:16:49 +0900

    Add files via upload

---
+++ 손글씨인식.py
@@ -0,0 +1,23 @@
+import sys, os
+from matplotlib.cbook import flatten
+sys.path.append(os.pardir)
+from dataset.mnist import load_mnist
+import numpy as np
+from PIL import Image
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
+
+(x_train, t_train), (x_test, t_test) = \
+    load_mnist(flatten = True, normalize = False)
+
+img = x_train[0]
+label = t_train[0]
+print(label)
+
+print(img.shape)
+img = img.reshape(28, 28)
+print(img.shape)
+
+img_show(img)
