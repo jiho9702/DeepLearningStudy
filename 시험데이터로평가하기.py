d7db8801ec7494db0a6433443a7493faa84c6c20
이지호 <42643691+jiho9702@users.noreply.github.com>
2022-02-05 17:18:05 +0900

    수정필요..

--- 시험데이터로평가하기.py
+++ 시험데이터로평가하기.py
@@ -1,17 +1,27 @@
+import sys, os
+sys.path.append(os.pardir)
 import numpy as np
 from dataset.mnist import load_mnist
 from 학습알고리즘구현하기 import TwoLayerNet
+import matplotlib.pyplot as plt
 
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
-train_loss_list = []
+
+network = TwoLayerNet(input_size=784, hidden_size = 50, output_size = 10)
 
 iters_num = 10000
 train_size = x_train.shape[0]
 batch_size = 100
 learning_rate = 0.1
-network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+iter_per_epoch = max(train_size / batch_size, 1)
 
 for i in range(iters_num):
+    print("{}번째".format(i+1))
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
@@ -23,3 +33,21 @@ for i in range(iters_num):
 
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
+
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc : " + str(train_acc) + ", " + str(test_acc))
+
+# 그래프 그리기
+markers = {'train': 'o', 'test': 's'}
+x = np.arange(len(train_acc_list))
+plt.plot(x, train_acc_list, label='train acc')
+plt.plot(x, test_acc_list, label='test acc', linestyle='--')
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
+plt.ylim(0, 1.0)
+plt.legend(loc='lower right')
+plt.show()
